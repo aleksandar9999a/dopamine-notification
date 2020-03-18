57412ee0f5886cf1f04065b78ead2007e94ebe03
aleksandar9999a <aleksandar9999a@gmail.com>
2020-03-18 13:02:05 +0200

    Added MenuService

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,6 +4,7 @@ import { NavigationComponent } from './navigation/navigation.component';
 import { NotificationsComponent } from './notifications/notifications.component';
 import { NotificationComponent } from './notification/notification.component';
 import { ActionsComponent } from './actions/actions.component';
+import { MenuService } from './services/menu.service';
 
 
 
@@ -11,6 +12,9 @@ import { ActionsComponent } from './actions/actions.component';
   declarations: [NavigationComponent, NotificationsComponent, NotificationComponent, ActionsComponent],
   imports: [
     CommonModule
+  ],
+  providers: [
+    MenuService
   ],
   exports: [NavigationComponent, NotificationsComponent, ActionsComponent]
 })
