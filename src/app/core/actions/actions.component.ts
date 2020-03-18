efe0838f771ec462bda110ce9c030c5b7dfc576e
aleksandar9999a <aleksandar9999a@gmail.com>
2020-03-18 14:56:53 +0200

    Added FirestoreService and add notifications

--- src/app/core/actions/actions.component.ts
+++ src/app/core/actions/actions.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FirestoreService } from '../services/firestore.service';
 
 @Component({
   selector: 'app-actions',
@@ -7,7 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActionsComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private fs: FirestoreService
+  ) { }
+
+  addTextNotification() {
+    return this.fs.addDoc({
+      type: 'text',
+      title: 'Test notification',
+      text: 'Test text notification',
+      expires: 5
+    })
+  }
+
+  addBonusNotification() {
+    return this.fs.addDoc({
+      type: 'Promotion',
+      image: 'https://www.freeiconspng.com/uploads/leistungen-promotion-icon-png-0.png',
+      title: '%30 off on sports betting',
+      link: 'https://www.google.com/',
+
+    })
+  }
+
+  addPromotionNotification() {
+    return this.fs.addDoc({
+      type: 'bonus',
+      title: 'You win a bonus!',
+      requirement: 'Deposit $50 to win',
+      expires: 3600
+    })
+  }
 
   ngOnInit(): void {
   }
