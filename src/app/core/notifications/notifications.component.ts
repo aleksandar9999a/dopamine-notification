28b4426121d856b8c3cf3d1179f869396b6fe52d
aleksandar9999a <aleksandar9999a@gmail.com>
2020-03-18 13:02:37 +0200

    Added animation

--- src/app/core/notifications/notifications.component.ts
+++ src/app/core/notifications/notifications.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { fade } from './notifications.animations';
+import { MenuService } from '../services/menu.service';
 
 @Component({
   selector: 'app-notifications',
   templateUrl: './notifications.component.html',
-  styleUrls: ['./notifications.component.css']
+  styleUrls: ['./notifications.component.css'],
+  animations: [fade]
 })
 export class NotificationsComponent implements OnInit {
+  isOpen = false;
 
-  constructor() { }
+  constructor(private menuService: MenuService) { }
 
   ngOnInit(): void {
+    this.menuService.state.subscribe((newState: boolean) => this.isOpen = newState);
   }
 
 }
