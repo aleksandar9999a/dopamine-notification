44f76389f5a9e4055b07c8cc0b2842cd996d1529
aleksandar9999a <aleksandar9999a@gmail.com>
2020-03-18 15:35:52 +0200

    Added time

--- src/app/core/actions/actions.component.ts
+++ src/app/core/actions/actions.component.ts
@@ -13,30 +13,35 @@ export class ActionsComponent implements OnInit {
   ) { }
 
   addTextNotification() {
+    const time = new Date();
     return this.fs.addDoc({
       type: 'text',
       title: 'Test notification',
       text: 'Test text notification',
-      expires: 5
+      expires: 5,
+      time
     })
   }
 
   addBonusNotification() {
+    const time = new Date();
     return this.fs.addDoc({
       type: 'Promotion',
       image: 'https://www.freeiconspng.com/uploads/leistungen-promotion-icon-png-0.png',
       title: '%30 off on sports betting',
       link: 'https://www.google.com/',
-
+      time
     })
   }
 
   addPromotionNotification() {
+    const time = new Date();
     return this.fs.addDoc({
       type: 'bonus',
       title: 'You win a bonus!',
       requirement: 'Deposit $50 to win',
-      expires: 3600
+      expires: 3600,
+      time
     })
   }
 
