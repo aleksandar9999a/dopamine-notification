31b6cbca42406e8c63813a7843a74ecc670555a9
aleksandar9999a <aleksandar9999a@gmail.com>
2020-03-18 13:03:36 +0200

    Toggle notifications

--- src/app/core/navigation/navigation.component.ts
+++ src/app/core/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuService } from '../services/menu.service';
 
 @Component({
   selector: 'app-navigation',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
-  constructor() { }
+  constructor(private menuService: MenuService) { }
+
+  toggle() {
+    return this.menuService.toggle();
+  }
 
   ngOnInit(): void {
   }
