89da5bbfefd23ca1559c23929cd0f68d6dfafe3a
aleksandar9999a <aleksandar9999a@gmail.com>
2020-03-18 10:08:35 +0200

    Added styles and notification component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavigationComponent } from './navigation/navigation.component';
 import { NotificationsComponent } from './notifications/notifications.component';
+import { NotificationComponent } from './notification/notification.component';
 
 
 
 @NgModule({
-  declarations: [NavigationComponent, NotificationsComponent],
+  declarations: [NavigationComponent, NotificationsComponent, NotificationComponent],
   imports: [
     CommonModule
   ],
