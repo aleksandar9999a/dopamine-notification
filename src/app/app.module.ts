cd9b97c9ff6b78bbfbfa782be08b24bccc43d029
aleksandar9999a <aleksandar9999a@gmail.com>
2020-03-18 05:57:10 +0200

    Added Core module and navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Core } from './core/common.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    Core
   ],
   providers: [],
   bootstrap: [AppComponent]
