768f1c1ccf5c9940d6512db39a277d8c6423021e
aleksandar9999a <aleksandar9999a@gmail.com>
2020-03-18 12:32:21 +0200

    Export action buttons

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,14 +3,15 @@ import { CommonModule } from '@angular/common';
 import { NavigationComponent } from './navigation/navigation.component';
 import { NotificationsComponent } from './notifications/notifications.component';
 import { NotificationComponent } from './notification/notification.component';
+import { ActionsComponent } from './actions/actions.component';
 
 
 
 @NgModule({
-  declarations: [NavigationComponent, NotificationsComponent, NotificationComponent],
+  declarations: [NavigationComponent, NotificationsComponent, NotificationComponent, ActionsComponent],
   imports: [
     CommonModule
   ],
-  exports: [NavigationComponent, NotificationsComponent]
+  exports: [NavigationComponent, NotificationsComponent, ActionsComponent]
 })
 export class Core { }
