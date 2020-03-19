121d87f56ab0bb7a0e5292b4f21b94e5950142f6
aleksandar9999a <aleksandar9999a@gmail.com>
2020-03-19 12:30:11 +0200

    Remove animations

--- src/app/core/notification/notification.component.ts
+++ src/app/core/notification/notification.component.ts
@@ -1,13 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { INotification } from 'src/app/interfaces/notification.interface';
 import { FirestoreService } from '../services/firestore.service';
-import { move } from './notification.animations';
 
 @Component({
   selector: 'app-notification',
   templateUrl: './notification.component.html',
-  styleUrls: ['./notification.component.css'],
-  animations: [move]
+  styleUrls: ['./notification.component.css']
 })
 export class NotificationComponent implements OnInit {
   @Input() data: INotification;
