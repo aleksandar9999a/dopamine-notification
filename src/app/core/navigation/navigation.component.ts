d7f0e80d3404d3f714a9d65bed47f60aa0f9f623
aleksandar9999a <aleksandar9999a@gmail.com>
2020-03-19 12:12:00 +0200

    Added counter for notifications and filter bonus

--- src/app/core/navigation/navigation.component.ts
+++ src/app/core/navigation/navigation.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuService } from '../services/menu.service';
+import { FirestoreService } from '../services/firestore.service';
+import { INotification } from 'src/app/interfaces/notification.interface';
 
 @Component({
   selector: 'app-navigation',
@@ -7,8 +9,20 @@ import { MenuService } from '../services/menu.service';
   styleUrls: ['./navigation.component.css']
 })
 export class NavigationComponent implements OnInit {
+  length: number = 0;
 
-  constructor(private menuService: MenuService) { }
+  constructor(
+    private menuService: MenuService,
+    private fs: FirestoreService
+  ) {
+    fs.getNotifications().forEach(arr => {
+      this.length = this.filterNotifications(arr).length;
+    });
+  }
+
+  filterNotifications(arr: any) {
+    return arr.filter(not => not.type !== 'bonus')
+  }
 
   toggle() {
     return this.menuService.toggle();
